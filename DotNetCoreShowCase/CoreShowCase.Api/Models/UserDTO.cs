d91e5ed6ec089f9a134ea5a312964c68a9d6cb4c
s.madonna <salvatore.m@gmail.com>
2018-05-28 14:40:43 +0200

    Added Models and Controllers

---
+++ DotNetCoreShowCase/CoreShowCase.Api/Models/UserDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreShowCase.Api.Models
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public string EMail { get; set; }
+        public string Password { get; set; }
+    }
+}
