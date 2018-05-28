1da44e22ce6f864ae6948a9214b52fb6c37c61ec
s.madonna <salvatore.m@gmail.com>
2018-05-28 16:04:28 +0200

    Minor changed

---
+++ DotNetCoreShowCase/CoreShowCase.Api/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreShowCase.Api.Entities
+{
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string Lastname { get; set; }
+        [Required]
+        public string EMail { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
