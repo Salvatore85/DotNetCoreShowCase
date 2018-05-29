90ed528a8daf86f01b0568b58495cc6ea578f076
s.madonna <salvatore.m@gmail.com>
2018-05-29 10:17:17 +0200

    Added extra model and AutoMapper

---
+++ DotNetCoreShowCase/CoreShowCase.Api/Models/BlogPostDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreShowCase.Api.Models
+{
+    public class BlogPostDTO
+    {
+        public int Id { get; set; }
+        public DateTime DatePosted { get; set; }
+        public string Title { get; set; }
+        public string Article { get; set; }
+    }
+}
