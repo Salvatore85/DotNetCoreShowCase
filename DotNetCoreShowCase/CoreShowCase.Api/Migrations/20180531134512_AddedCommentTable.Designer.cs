f38e77e3d484ad4d2b10edb466fe652600f19890
s.madonna <salvatore.m@gmail.com>
2018-05-31 16:01:21 +0200

    Minor changes

--- DotNetCoreShowCase/CoreShowCase.Api/Migrations/20180531134512_AddedCommentTable.Designer.cs
+++ DotNetCoreShowCase/CoreShowCase.Api/Migrations/20180531134512_AddedCommentTable.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace CoreShowCase.Api.Migrations
 {
     [DbContext(typeof(CoreShowCaseContext))]
-    partial class CoreShowCaseContextModelSnapshot : ModelSnapshot
+    [Migration("20180531134512_AddedCommentTable")]
+    partial class AddedCommentTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DotNetCoreShowCase/CoreShowCase.Api/Migrations/20180531134512_AddedCommentTable.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CoreShowCase.Api.Migrations
+{
+    public partial class AddedCommentTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Approved = table.Column<bool>(nullable: false),
+                    BlogId = table.Column<int>(nullable: false),
+                    BlogPostId = table.Column<int>(nullable: true),
+                    DatePosted = table.Column<DateTime>(nullable: false),
+                    TheComment = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_BlogPosts_BlogPostId",
+                        column: x => x.BlogPostId,
+                        principalTable: "BlogPosts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_BlogPostId",
+                table: "Comments",
+                column: "BlogPostId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+        }
+    }
+}
